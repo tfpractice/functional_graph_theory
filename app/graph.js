32a9fd22e1575046b847fbe6295b54148b2b8323
theory friction practice <jackson.brebnor@gmail.com>
2016-09-21 21:44:32 -0400

    basic accessors

--- app/graph.js
+++ app/graph.js
@@ -1,15 +1,51 @@
+let makeEdges = (...elements) =>
+	elements.reduce((eMap, next) =>
+		eMap.set(next, new Map()), new Map());
+
+let makeGraph = (...elements) => ({
+	nodes: new Set(elements),
+	edges: makeEdges(...elements),
+});
+
+let nodes = ({ nodes = new Set() }) => nodes;
+let edges = ({ edges = new Map() }) => edges;
+let neighbors = ({ edges }) => (node) => edges.get(node);
+let contains = ({ nodes }) => (node) => nodes.has(node);
+let clearNodes = ({ nodes }) => nodes.clear;
+let clearEdges = ({ edges }) => edges.clear;
+let isAdjacent = ({ edges }) => (n0) => (n1) =>
+	edges.get(n0).has(n1);
+// let spreadKeys = (map) => [...map.keys()];
+// let spreadValues = (map) => [...map.values()];
+// let spreadEntries = (map) => [...map.entries()];
+// let lastKey = (map) => spreadKeys(map).pop();
+
 let Graph = (...elements) => {
-	// let gState = makeGraph(...elements);
-	// return {
-	//     nodes: nodes(gState),
-	//     edges: edges(gState),
-	//     neighbors: neighbors(gState),
-	//     contains: contains(gState),
-	//     addEdge: addEdge(gState),
-	//     removeEdge: removeEdge(gState),
-	//     isAdjacent: isAdjacent(gState),
-	// };
-	return { nodes: new Set(elements) };
+	let gState = makeGraph(...elements);
+	return {
+		nodes: nodes(gState),
+		edges: edges(gState),
+		neighbors: neighbors(gState),
+		contains: contains(gState),
+		clearEdges: clearEdges(gState),
+		// addEdge: addEdge(gState),
+		// removeEdge: removeEdge(gState),
+		isAdjacent: isAdjacent(gState),
+	};
+	// return { nodes: new Set(elements) };
 };
 
+// let operators = { makeEdges, makeGraph, nodes, edges, neighbors, contains };
 module.exports = Graph;
+module.exports.makeEdges = makeEdges;
+module.exports.makeGraph = makeGraph;
+module.exports.nodes = nodes;
+module.exports.edges = edges;
+module.exports.neighbors = neighbors;
+module.exports.contains = contains;
+module.exports.clearNodes = clearNodes;
+module.exports.clearEdges = clearEdges;
+module.exports.isAdjacent = isAdjacent;
+
+
+//
--- spec/graph.spec.js
+++ spec/graph.spec.js
@@ -1,10 +1,14 @@
 fdescribe('Graph', function() {
-	let Graph;
 	let Node;
 	let n0, n1, n2, n3, n4, n5, n6;
+	let myNodes, xNodes;
 	let myGraph;
 	beforeAll(function() {
 		({ Graph } = this.App);
+		({ makeEdges, makeGraph, nodes, edges } = Graph);
+		({ neighbors, contains, isAdjacent } = Graph);
+		({ clearNodes, clearEdges } = Graph);
+
 		Node = (label = '', data = {}) => ({
 			label,
 			data,
@@ -14,21 +18,59 @@ fdescribe('Graph', function() {
 	});
 
 	beforeEach(function() {
-		let allNodes = Array(7).fill('node').map((el, id) =>
+		myNodes = Array(7).fill('node').map((el, id) =>
 			Node(`${el}::${id}`, { position: id })
 		);
-		[n0, n1, n2, n3, n4, n5, n6] = allNodes;
-		myGraph = Graph(...allNodes);
+		altNodes = Array(7).fill('altnode').map((el, id) =>
+			Node(`${el}::${id}`, { position: id })
+		);
+		[n0, n1, n2, n3, n4, n5, n6] = myNodes;
+		myGraph = Graph(...myNodes);
 	});
 
 	it('is a function', function() {
 		expect(Graph).toBeFunction();
 	});
 
-	// describe('when given nodes', () => {
-	// 	it('returns a set of objects', function() {
-	// 		console.log(myGraph);
-	// 		expect(myGraph.nodes instanceof Set).toBeTrue();
-	// 	});
-	// });
+	describe('when given a collection of elements', () => {
+		describe('makeEdges(...elements)', () => {
+			it('returns a new Map of those elements', () => {
+				expect(makeEdges(...myNodes) instanceof Map).toBeTrue();
+			});
+		});
+		describe('makeGraph(...elements)', () => {
+			it('returns a new object with nodes and edges', () => {
+				expect(makeGraph(...myNodes)).toBeObject();
+			});
+		});
+	});
+	describe('accessors', () => {
+		describe('nodes', () => {
+			it('returns a set of the nodes from that graph', () => {
+				expect(nodes(myGraph) instanceof Set).toBeTrue();
+			});
+		});
+		describe('edges', () => {
+			it('returns a set of the nodes from that graph', () => {
+				expect(edges(myGraph) instanceof Map).toBeTrue();
+			});
+		});
+		describe('neighbors(edge)(node)', () => {
+			it('returns a map entry of that nodes neighbors ', function() {
+				expect(neighbors(myGraph)(n0) instanceof Map).toBeTrue();
+
+			});
+		});
+		describe('contains(graph)(node)', () => {
+			it('checks for the presence of a node in the graph', function() {
+				expect(contains(myGraph)(n0)).toBeTrue();
+			});
+		});
+		describe('isAdjacent(graph)(n0)(n1)', () => {
+			it('checks for the presence of a node in the graph', function() {
+				expect(isAdjacent(myGraph)(n0)(n1)).toBeFalse();
+			});
+		});
+	});
+
 });
