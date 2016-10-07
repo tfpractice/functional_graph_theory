36abc06de872651f7c6a1cc881a7512a1af806fc
theory friction practice <jackson.brebnor@gmail.com>
2016-10-07 10:47:53 -0400

    pre merge

--- app/edge/edge_reducers.js
+++ app/edge/edge_reducers.js
@@ -1,5 +1,5 @@
 const appendNew = (edges = new Map) => (src) =>
-    edges.has(src) ? edges : edges.set(src, new Map);
+	edges.has(src) ? edges : edges.set(src, new Map);
 
 const appendR = (edges = new Map, src) => appendNew(edges)(src);
 
@@ -8,8 +8,9 @@ const rmNodeR = (edges = new Map, src) => edges.delete(src) ? edges : edges;
 const addNeighborR = (nabes = new Map, n, w = 0) => nabes.set(n, w);
 
 const addEntryR = (nabes = new Map, [n, w = 0]) => addNeighborR(nabes, n, w);
+const addEdgeR = (nabes = new Map, [src, n, w = 0]) => addNeighborR(nabes, n, w);
 
 const mergeNeighborsR = (nabes = new Map, alts = new Map) =>
-    mergeNeighbors(nabes)(alts);
+	mergeNeighbors(nabes)(alts);
 
 module.exports = { appendNew, appendR, rmNodeR, addNeighborR, addEntryR };
