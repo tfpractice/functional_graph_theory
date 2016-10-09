2ea762679e28c81f038a5014fe923b9c904f5b38
theory friction practice <jackson.brebnor@gmail.com>
2016-10-09 18:01:41 -0400

    spec helper refactoring

--- spec/helpers/spec_helper.js
+++ spec/helpers/spec_helper.js
@@ -9,22 +9,12 @@ beforeAll(function() {
 		toString: () =>
 			label,
 	});
-	eFilter = (coll) => coll.filter(({ data }) => data.position % 2 ===
-		0);
-	oFilter = (coll) => coll.filter(({ data }) => data.position % 2 ===
-		1);
-
-	nEdgesR = (edges) => eFilter(Array.from(Edge.nodes(edges)))
-		.reduce((prev, next, id) => {
-			Edge.addEdges(edges)(prev, id * 2)(next);
-			return next;
-		});
-
-	oEdgesR = (edges) => oFilter(Array.from(Edge.nodes(edges)))
-		.reduce((prev, next, id) => {
-			Edge.addEdges(edges)(prev, id * 2)(next);
-			return next;
-		});
+	eFilter = (coll) => coll.filter(({ data: { position: p } }) => p % 2 === 0);
+	oFilter = (coll) => coll.filter(({ data: { position: p } }) => p % 2 === 1);
+	nEdgesR = (edges) => eFilter(Graph.nodes(edges))
+		.reduce((prv, nxt, id) => Graph.addEdges(edges)(prv, id * 2)(nxt) && nxt);
+	oEdgesR = (edges) => oFilter(Graph.nodes(edges))
+		.reduce((prv, nxt, id) => Graph.addEdges(edges)(prv, id * 2)(nxt) && nxt);
 });
 
 beforeEach(function() {
@@ -40,28 +30,28 @@ beforeEach(function() {
 	[n10, n11, n12, n13, n14, n15, n16, n17, n18, n19] = lastTen;
 	eNodes = eFilter(lastTen);
 	oNodes = oFilter(lastTen);
-	myGraphR = Edge.fromElements(...firstTen);
-	altGraphR = Edge.fromElements(n4, n5, n6, n7, n8, n9);
-	evenGraphR = Edge.fromElements(...firstTen, ...eNodes);
-	oddGraphR = Edge.fromElements(...firstTen, ...oNodes);
+	myGraphR = Graph.fromElements(...firstTen);
+	altGraphR = Graph.fromElements(n4, n5, n6, n7, n8, n9);
+	evenGraphR = Graph.fromElements(...firstTen, ...eNodes);
+	oddGraphR = Graph.fromElements(...firstTen, ...oNodes);
 
-	Edge.addEdges(myGraphR)(n0, 2)(n1, n2);
-	Edge.addEdges(myGraphR)(n1, 4)(n4, n2);
-	Edge.addEdges(myGraphR)(n1, 6)(n6);
-	Edge.addEdges(myGraphR)(n2, 3)(n3);
-	Edge.addEdges(myGraphR)(n5, 4)(n4);
-	Edge.addEdges(myGraphR)(n3, 8)(n4);
-	Edge.addEdges(myGraphR)(n5, 7)(n6);
+	Graph.addEdges(myGraphR)(n0, 2)(n1, n2);
+	Graph.addEdges(myGraphR)(n1, 4)(n4, n2);
+	Graph.addEdges(myGraphR)(n1, 6)(n6);
+	Graph.addEdges(myGraphR)(n2, 3)(n3);
+	Graph.addEdges(myGraphR)(n5, 4)(n4);
+	Graph.addEdges(myGraphR)(n3, 8)(n4);
+	Graph.addEdges(myGraphR)(n5, 7)(n6);
 
-	Edge.addEdges(evenGraphR)(n0, 11)(n1);
-	Edge.addEdges(evenGraphR)(n0, 22)(n9);
+	Graph.addEdges(evenGraphR)(n0, 11)(n1);
+	Graph.addEdges(evenGraphR)(n0, 22)(n9);
 	nEdgesR(evenGraphR);
 
-	Edge.addEdges(oddGraphR)(n0, 11)(n1);
-	Edge.addEdges(oddGraphR)(n0, 22)(n9);
+	Graph.addEdges(oddGraphR)(n0, 11)(n1);
+	Graph.addEdges(oddGraphR)(n0, 22)(n9);
 	oEdgesR(oddGraphR);
 
-	myEdgesR = Edge.spawn(myGraphR);
-	oddEdgesR = Edge.spawn(oddGraphR);
-	evenEdgesR = Edge.spawn(evenGraphR);
+	myEdgesR = Graph.spawn(myGraphR);
+	oddEdgesR = Graph.spawn(oddGraphR);
+	evenEdgesR = Graph.spawn(evenGraphR);
 });
