5a4dd29a82ce24bcb8e6012714bc5afe56ae3eb1
theory friction practice <jackson.brebnor@gmail.com>
2016-10-09 18:47:53 -0400

    short circuit neighbor map

--- app/graph.js
+++ app/graph.js
@@ -3,15 +3,15 @@ const { spreadKeys, spreadValues, spreadEntries } = utils;
 const { hasKey, x_hasKey, showGraph } = utils;
 const Reducers = require('./reducers');
 const {
-    addEdgeR,
-    appendR,
-    rmNodeR,
-    addNeighborR,
-    addEntryR,
-    removeEdgeR,
-    removeNeighborsR,
-    rmNodeXR,
-    mergeEdgesR,
+	addEdgeR,
+	appendR,
+	rmNodeR,
+	addNeighborR,
+	addEntryR,
+	removeEdgeR,
+	removeNeighborsR,
+	rmNodeXR,
+	mergeEdgesR,
 } =
 Reducers;
 
@@ -25,62 +25,62 @@ const copy = spawn;
 const fromElements = (...elements) => addNodes(spawn())(...elements);
 
 const contains = (edges = new Map) => (node) => edges.has(node);
-const nodes = (edges = new Map) => [...new Set(edges.keys())];
+const nodes = (edges = new Map) => [...new Set(spreadKeys(edges))];
 
-const adj = (edges = new Map) => (src) =>
-    edges.has(src) ? edges.get(src) : new Map;
+const adj = (edges = new Map) => (src) => edges.get(src) || new Map;
 
 const neighbors = (edges = new Map) => (src) => nodes(adj(edges)(src));
 
 const isAdjacent = (edges = new Map) => (src) => (nabe) =>
-    contains(adj(edges)(src))(nabe);
+	contains(adj(edges)(src))(nabe);
 
 const clearEdges = (edges) => edges.clear;
 
-const addNodes = (edges = new Map) => (...nodes) => nodes.reduce(appendR, edges);
+const addNodes = (edges = new Map) => (...nodes) => nodes.reduce(appendR,
+	edges);
 
 const removeNodes = (edges = new Map) => (...ns) => ns.reduce(rmNodeXR, edges);
 
 const addNeighbor = (edges = new Map) => (src) => (n, w = 0) =>
-    addNeighborR(adj(edges)(src), n, w);
+	addNeighborR(adj(edges)(src), n, w);
 
 const addEdges = (edges = new Map) => (src, w = 0) => (...nabes) =>
-    nabes.map(edgeEntry(w)(src)).reduce(addEdgeR, edges);
+	nabes.map(edgeEntry(w)(src)).reduce(addEdgeR, edges);
 
 const removeNeighbors = (edges = new Map) => (...nodes) =>
-    nodes.reduce(removeNeighborsR, edges);
+	nodes.reduce(removeNeighborsR, edges);
 
 const removeEdges = (edges = new Map) => (src) => (...nabes) =>
-    nabes.map(edgeEntry(0)(src)).reduce(removeEdgeR, edges);
+	nabes.map(edgeEntry(0)(src)).reduce(removeEdgeR, edges);
 
 const addEntry = (nabes = new Map) => ([n, w = 0]) => addNeighborR(nabes, n,
-    w);
+	w);
 
 const mergeNeighbors = (nabes = new Map) => (altNabes = new Map) =>
-    [...altNabes].reduce(addEntryR, nabes);
+	[...altNabes].reduce(addEntryR, nabes);
 
 const mergeEdges = (edges = new Map) => (altEdges = new Map) => {
-    [...altEdges].reduce(mergeEdgesR, edges);
+	[...altEdges].reduce(mergeEdgesR, edges);
 };
 
 module.exports = {
-    spawn,
-    contains,
-    nodes,
-    adj,
-    copy,
-    isAdjacent,
-    addNodes,
-    removeEdges,
-    removeNodes,
-    neighbors,
-    addNeighbor,
-    addEdges,
-    addEdgeR,
-    addEntry,
-    weighedEntry,
-    removeNeighbors,
-    mergeNeighbors,
-    mergeEdges,
-    fromElements,
+	spawn,
+	contains,
+	nodes,
+	adj,
+	copy,
+	isAdjacent,
+	addNodes,
+	removeEdges,
+	removeNodes,
+	neighbors,
+	addNeighbor,
+	addEdges,
+	addEdgeR,
+	addEntry,
+	weighedEntry,
+	removeNeighbors,
+	mergeNeighbors,
+	mergeEdges,
+	fromElements,
 };
