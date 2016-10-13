94cce9d90538255bbf7cbc98349a8594581fedc2
theory friction practice <jackson.brebnor@gmail.com>
2016-10-13 11:43:36 -0400

    folds commands into module/spec path

--- app/utils/index.js
+++ app/utils/index.js
@@ -3,10 +3,4 @@ const Comparitors = require('./comparitors');
 const Commands = require('./commands');
 const Strings = require('./strings');
 
-// const addSet = (coll = new Set, elem) => coll.add(elem);
-// const addMap = (coll = new Map, [k, v]) => coll.set(k, v);
-// const rmColl = (coll = new Set, elem) => coll.delete(elem) ? set : set;
-// module.exports = Object.assign({ Queries, Comparitors, Strings }, Queries,
-//  Comparitors, Strings);
-
 module.exports = { Queries, Comparitors, Strings, Commands };
