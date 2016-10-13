b98ea134f40c486737f7958acc5b611e8420e6de
theory friction practice <jackson.brebnor@gmail.com>
2016-10-13 10:47:25 -0400

    removes index code

--- app/utils/index.js
+++ app/utils/index.js
@@ -6,4 +6,4 @@ const addSet = (coll = new Set, elem) => coll.add(elem);
 const addMap = (coll = new Map, [k, v]) => coll.set(k, v);
 const rmColl = (coll = new Set, elem) => coll.delete(elem) ? set : set;
 
-module.exports = Object.assign({}, queries, comparitors, strings, );
+module.exports = Object.assign({}, queries, comparitors, strings);
