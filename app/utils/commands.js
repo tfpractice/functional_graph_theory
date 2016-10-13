3a8990525c1ec181eddd4c5b8644d4098ad1a1c0
theory friction practice <jackson.brebnor@gmail.com>
2016-10-13 12:02:34 -0400

    utils.commands

--- app/utils/commands.js
+++ app/utils/commands.js
@@ -1,5 +1,8 @@
+const tuple = (val) => (key) => [key, val];
+const triple = (val) => (key0) => (key1) => [key0, key1, val];
+
 const addSet = (coll = new Set, elem) => coll.add(elem);
 const addMap = (coll = new Map, [k, v]) => coll.set(k, v);
 const rmColl = (coll = new Set, elem) => coll.delete(elem) ? coll : coll;
 
-module.exports = { addSet, addMap, rmColl, };
+module.exports = { tuple, triple, addSet, addMap, rmColl, };
--- spec/utils/commands.spec.js
+++ spec/utils/commands.spec.js
@@ -1,31 +1,41 @@
-fdescribe('Utils/Commands', function() {
-    beforeAll(function() {
+fdescribe('Utils/Commands', function () {
+    beforeAll(function () {
         console.log('\n.........U/Commands Spec.........');
     });
 
     describe('comparitors', () => {
         beforeEach(() => {
-            set0 = new Set([1, 2, 3]);
-            set1 = new Set([2, 3, 4]);
+            cSet = new Set([ 1, 2, 3, 4 ]);
+            cMap = new Map().set(8, 1).set(7, 2).set(6, 3).set(5, 4);
+            cArr = [ 00, 11, 22, 33, 44 ];
         });
-
-        // describe('inter', () => {
-        //     it('returns an array of all shared elements ', () => {
-        //         expect(Comp.inter(set0)(set1)).toBeArray();
-        //         expect(Comp.inter(set0)(set1)).toContain(2, 3);
-        //     });
-        // });
-        // describe('diff', () => {
-        //     it('returns elements in first set absent fom the second ', () => {
-        //         expect(Comp.diff(set0)(set1)).toBeArray();
-        //         expect(Comp.diff(set0)(set1)).toContain(1, 4);
-        //     });
-        // });
-        // describe('union', () => {
-        //     it('combines elements of both sets ', () => {
-        //         expect(Comp.union(set0)(set1)).toBeArray();
-        //         expect(Comp.union(set0)(set1)).toContain(1, 2, 3, 4);
-        //     });
-        // });
+		describe('tuple(val)(key)', () => {
+            it('returns a [key, val] array', () => {
+                expect(Comm.tuple(0)('zero')).toBeArray();
+            });
+        });
+        describe('triple(val)(key)(key1)', () => {
+            it('returns a [key0,key1,val] array', () => {
+                expect(Comm.triple(1)('zero')('one')).toBeArray();
+            });
+        });
+        describe('addSet(val)', () => {
+            it('adds a value to a set', () => {
+                Comm.addSet(cSet, 9);
+                expect([ ...cSet ]).toContain(9);
+            });
+        });
+        describe('addMap(tuple)', () => {
+            it('appends a [k,v] tuple to a map', () => {
+                Comm.addMap(cMap, [ 22, 33 ]);
+                expect([ ...cMap.keys() ]).toContain(22);
+            });
+        });
+        describe('rmCollection', () => {
+            it('returns a collection without the element', () => {
+                Comm.rmColl(cSet, 2);
+                expect([ ...cSet ]).not.toContain(2);
+            });
+        });
     });
 });
