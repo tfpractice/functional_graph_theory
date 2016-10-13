281fed6fa1c8f7b169f3a79787b4a2459146bf2d
theory friction practice <jackson.brebnor@gmail.com>
2016-10-13 11:19:51 -0400

    fixes string spec

--- spec/utils/strings.spec.js
+++ spec/utils/strings.spec.js
@@ -6,39 +6,39 @@ fdescribe('Utils/Strings', function() {
 	describe('strings', () => {
 		describe('redStr', () => {
 			it('concatenates the string with an  argument', () => {
-				expect(utils.redStr('1,2,3,', 6)).toBeString();
+				expect(Str.redStr('1,2,3,', 6)).toBeString();
 			});
 		});
 		describe('kString', () => {
 			it('concatenates the string representaion of all keys', () => {
-				expect(utils.kString([1, 2, 3, 6])).toBeString();
+				expect(Str.kString([1, 2, 3, 6])).toBeString();
 			});
 		});
 
 		describe('vString', () => {
 			it('concatenates the string representaion of all keys', () => {
-				expect(utils.vString(new Set([1, 2, 3, 6]))).toBeString();
+				expect(Str.vString(new Set([1, 2, 3, 6]))).toBeString();
 			});
 		});
 		describe('kvString', () => {
 			it('concatenates the string representaion of all keys', () => {
-				expect(utils.kvString([1, 2, 3, 6])).toBeString();
+				expect(Str.kvString([1, 2, 3, 6])).toBeString();
 			});
 		});
 		describe('collString', () => {
 			it('concatenates the string representaion of all keys', () => {
-				expect(utils.collString([1, 2, 3, 6])).toBeString();
+				expect(Str.collString([1, 2, 3, 6])).toBeString();
 			});
 		});
 		describe('pathString', () => {
 			it('should behave...', () => {
-				expect(utils.pathString(myGraphR)).toBeString();
+				expect(Str.pathString(myGraphR)).toBeString();
 			});
 		});
 		describe('edgeString', () => {
 			it('should behave...', () => {
-				let edge0 = utils.first(myGraphR);
-				expect(utils.edgeString(edge0)).toBeString();
+				let edge0 = Query.first(myGraphR);
+				expect(Str.edgeString(edge0)).toBeString();
 			});
 		});
 	});
