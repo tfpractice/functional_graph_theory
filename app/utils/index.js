c1834d30fc190f1b647e29c454de0f622ae46d25
theory friction practice <jackson.brebnor@gmail.com>
2016-10-13 10:35:30 -0400

    more queries

--- app/utils/index.js
+++ app/utils/index.js
@@ -9,33 +9,20 @@ const {
 	firstK,
 	lastK,
 	rmFirst,
+	hasK,
+	x_hasK,
+	hasKV,
+	x_hasKV,
 } = queries;
-// const spread = (coll = []) => [...coll];
-// const spreadK = (coll = []) => [...coll.keys()];
-// const spreadV = (coll = []) => [...coll.values()];
-// const spreadKV = (coll = []) => [...coll.entries()];
-
-// const first = (coll = []) => spread(coll).shift();
-// const last = (coll = []) => spread(coll).pop();
-// const fromIndex = (coll = new Set) => (index) => spread(coll).slice(index, 1);
-
-// const firstK = (coll = []) => first(spreadK(coll));
-// const lastK = (coll = []) => last(spreadK(coll));
-
-// let rmFirst = (coll = []) => {
-// 	let elem = first(coll);
-// 	coll.delete(elem);
-// 	return elem;
-// };
 
 const addSet = (coll = new Set, elem) => coll.add(elem);
 const addMap = (coll = new Map, [k, v]) => coll.set(k, v);
 const rmColl = (coll = new Set, elem) => coll.delete(elem) ? set : set;
 
-const hasK = (coll = []) => (key) => coll.has(key);
-const x_hasK = (coll = []) => (key) => !coll.has(key);
-const hasKV = (path) => ([key, val]) => path.has(key);
-const x_hasKV = (path) => ([key, val]) => !hasKV(path)([key, val]);
+// const hasK = (coll = []) => (key) => coll.has(key);
+// const x_hasK = (coll = []) => (key) => !coll.has(key);
+// const hasKV = (path) => ([key, val]) => path.has(key);
+// const x_hasKV = (path) => ([key, val]) => !hasKV(path)([key, val]);
 
 const inter = (c0) => (c1) => spread(c0).filter(hasK(c1));
 const diff = (c0) => (c1) => spread(c0).filter(x_hasK(c1));
@@ -56,37 +43,36 @@ let componentString = ([node, set]) =>
 
 let graphString = (edges) =>
 	spreadKV(edges).reduce((str, [node, nabes], id) =>
-		str + edgeString([node, nabes]),
+		str + edgeString([ node, nabes ]),
 		'Showing Edges\n');
 
 let showGraph = ({ edges }) => (graphString(edges));
 
 module.exports = Object.assign({}, queries, {
-	// spread,
-	// spreadK,
-	// spreadV,
-	// spreadKV,
-	// first,
-	// last,
-	// firstK,
-	// lastK,
-	// rmFirst,
-	hasK,
-	x_hasK,
-	hasKV,
-	x_hasKV,
-	redStr,
-	collString,
-	kString,
-	vString,
-	kvString,
+    // spread,
+    // spreadK,
+    // spreadV,
+    // spreadKV,
+    // first,
+    // last,
+    // firstK,
+    // lastK,
+    // rmFirst,
+    hasK,
+    x_hasK,
+    hasKV,
+    x_hasKV,
+    redStr,
+    collString,
+    kString,
+    vString,
+    kvString,
 
-	showGraph,
-	inter,
-	diff,
-	union,
-	edgeString,
-	pathString,
-});
+    showGraph,
+    inter,
+    diff,
+    union,
+    edgeString,
+    pathString, });
 
 // module.exports = Object.assign({}, queries, comparitors, commands);
--- app/utils/queries.js
+++ app/utils/queries.js
@@ -1,5 +1,5 @@
-const spread = (coll = []) => [ ...coll ];
-const spreadK = (coll = []) => [ ...coll.keys() ];
+const spread = (coll = []) => [...coll];
+const spreadK = (coll = []) => [...coll.keys()];
 const spreadV = (coll = []) => [...coll.values()];
 const spreadKV = (coll = []) => [...coll.entries()];
 
@@ -10,19 +10,30 @@ const fromIndex = (coll = new Set) => (index) => spread(coll).slice(index, 1);
 const firstK = (coll = []) => first(spreadK(coll));
 const lastK = (coll = []) => last(spreadK(coll));
 
+const hasK = (coll = []) => (key) => coll.has(key);
+const x_hasK = (coll = []) => (key) => !coll.has(key);
+const hasKV = (path) => ([key, val]) => path.has(key);
+const x_hasKV = (path) => ([key, val]) => !hasKV(path)([key, val]);
+
 const rmFirst = (coll = []) => {
-    let elem = first(coll);
-    coll.delete(elem);
-    return elem;
+	let elem = first(coll);
+	coll.delete(elem);
+	return elem;
 };
 
-module.exports = { spread,
-    spreadK,
-    spreadV,
-    spreadKV,
-    first,
-    last,
-    fromIndex,
-    firstK,
-    lastK,
-    rmFirst, };
+module.exports = {
+	spread,
+	spreadK,
+	spreadV,
+	spreadKV,
+	first,
+	last,
+	fromIndex,
+	firstK,
+	lastK,
+	hasK,
+	x_hasK,
+	hasKV,
+	x_hasKV,
+	rmFirst,
+};
