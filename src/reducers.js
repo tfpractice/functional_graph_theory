8a5949e1aa102e0a66522fdd7c6bdaac1457fea6
theory friction practice <jackson.brebnor@gmail.com>
2017-01-01 19:35:23 -0500

    reducers specs

--- src/reducers.js
+++ src/reducers.js
@@ -1,17 +1,19 @@
-const set = m => k => v => new Map(m).set(k, v);
-
 import { collections, } from 'turmeric';
 const { default: spread, spreadK } = collections.spread;
 const { group: { tuple, triple, flatTuple }} = collections;
 const { manipulate: { addMap }, } = collections;
+const { accessors: { get }, } = collections;
+
+// const { accessors: { get }, } = collections;
 
 const Utils = require('./utils');
 const { Commands: { rmColl, }} = Utils;
 const { Comparitors: { uniteMap, mapDiff, mapUnion, diff }} = Utils;
 
-const get = m => k => new Map(m).get(k);
+// const get = m => k => new Map(m).get(k);
+const set = m => k => v => new Map(m).set(k, v);
 
-const nMap = (edges = new Map) => src => new Map(get(edges)(src));
+const nMap = edges => src => new Map(get(edges)(src));
 const nabes = (edges = new Map) => src => spreadK(nMap(edges)(src));
 
 const addSrc = (edges, src) => addMap(edges)(src)(nMap(edges)(src));
