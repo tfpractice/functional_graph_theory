b0917a0f41cfb0baf3b25385e325adc58c0dcb05
theory friction practice <jackson.brebnor@gmail.com>
2017-01-04 01:12:46 -0500

    shared and graph tests

--- spec/graph.spec.js
+++ spec/graph.spec.js
@@ -1,137 +1,143 @@
 import 'jasmine-expect';
-import { spawn, } from 'src/graph';
+import { collections, } from 'turmeric';
+import { eFilter, myNodes, oFilter, } from './shared';
+import { n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, } from './shared';
+import { adj, copy, fromElements, neighbors, nodes, spawn, } from 'src/graph';
+import { addEdges, addNodes, contains, isAdjacent, removeNodes, } from 'src/graph';
+import { addEntry, addNeighbor, clearNeighbors, mergeEdges, removeEdges, } from 'src/graph';
+import { addEdgeBin, } from 'src/reducers';
+const { tuple } = collections;
+const myGraph = fromElements(...myNodes);
+const eGraph = fromElements(eFilter(myNodes));
+const oGraph = fromElements(oFilter(myNodes));
 
-// xdescribe('Graph', () => {
-//   beforeAll(() => {
-//     console.log('\n.........GraphR Spec.........');
-//   });
-//
-//   beforeEach(() => {
-//     rEdges = Graph.spawn();
-//     evens = Graph.addNodes()(...eFilter(myNodes));
-//     odds = Graph.addNodes()(...oFilter(oNodes));
-//   });
-//
+// console.log('myGraph', eGraph);
+
 describe('spawn(nabes)', () => {
   it('returns a new Map', () => {
     expect(spawn() instanceof Map).toBeTrue();
   });
 });
 
-//   xdescribe('fromElements', () => {
-//     it('returns a new edgelist with the new nodes appended', () => {
-//       const sEdges = Graph.fromElements(...myNodes);
+describe('fromElements', () => {
+  it('returns a new edgelist with the new nodes appended', () => {
+    expect(fromElements(...myNodes) instanceof Map).toBeTrue();
+  });
+});
+
+describe('accessors', () => {
+  describe('nodes', () => {
+    it('returns an array of the nodes in the edgelist', () => {
+      expect(nodes(myGraph)).toBeArray();
+    });
+  });
+
+  describe('copy', () => {
+    it('returns a Map of nodes and neighbor', () => {
+      expect(copy(myGraph) instanceof Map).toBeTrue();
+    });
+  });
+
+  describe('adj', () => {
+    it('returns a map of all the neighboring nodes and weights', () => {
+      expect(adj(eGraph)(n2) instanceof Map).toBeTrue();
+    });
+  });
+
+  describe('neighbors(edge)(node)', () => {
+    it('returns a map entry of that nodes neighbors ', () => {
+      expect(neighbors(myGraph)(n0) instanceof Array).toBeTrue();
+    });
+  });
+
+  describe('contains', () => {
+    it('checks if a graph has a node', () => {
+      expect(contains(myGraph)(n4)).toBeTrue();
+    });
+  });
+});
+
+describe('operators', () => {
+  describe('isAdjacent(graph)(n0)(n1)', () => {
+    it('checks for the presence of a node in the graph', () => {
+      expect(isAdjacent(myGraph)(n0)(n6)).toBeFalse();
+    });
+  });
+
+  describe('addNodes', () => {
+    it('adds an entry to the edges Map', () => {
+      expect(addNodes(eGraph)(n3).has(n3)).toBeTrue();
+    });
+  });
+
+  describe('removeNodes', () => {
+    it('removes the nodes from the edges', () => {
+      expect((removeNodes(myGraph)(n0, n2)).has(n0)).toBeFalse();
+    });
+  });
+
+  describe('addEdges', () => {
+    it('adds an neighbor entry for each node', () => {
+      expect((addEdges(spawn())(n0)(n1, n2, n3)).has(n2)).toBeTrue();
+    });
+  });
+
 //
-//       expect(Graph.contains(sEdges)(n2)).toBeTrue();
-//     });
-//   });
-//   xdescribe('accessors', () => {
-//     xdescribe('nodes', () => {
-//       it('returns an array of the nodes in the edgelist', () => {
-//         expect(Graph.nodes(evens)).toBeArray();
-//       });
-//     });
-//     xdescribe('copy', () => {
-//       it('returns a Map of nodes and neighbor', () => {
-//         expect(Graph.copy(rEdges) instanceof Map).toBeTrue();
-//       });
-//     });
-//     xdescribe('adj', () => {
-//       it('returns a map of all the neighboring nodes and weights', () => {
-//         expect(Graph.adj(evens)(n2) instanceof Map).toBeTrue();
-//       });
-//     });
-//     xdescribe('neighbors(edge)(node)', () => {
-//       it('returns a map entry of that nodes neighbors ', () => {
-//         expect(Graph.neighbors(myGraphR)(n0) instanceof Array).toBeTrue();
-//       });
-//     });
-//     xdescribe('contains', () => {
-//       it('checks if a graph has a node', () => {
-//         expect(Graph.contains(evens)(n12)).toBeTrue();
-//       });
-//     });
-//   });
-//   xdescribe('operators', () => {
-//     xdescribe('isAdjacent(graph)(n0)(n1)', () => {
-//       it('checks for the presence of a node in the graph', () => {
-//         expect(Graph.isAdjacent(myGraphR)(n0)(n6)).toBeFalse();
-//       });
-//     });
-//
-//     xdescribe('addNodes', () => {
-//       it('adds an entry to the edges Map', () => {
-//         Graph.addNodes(rEdges)(n0);
-//         expect(rEdges.has(n0)).toBeTrue();
-//       });
-//     });
-//     xdescribe('removeNodes', () => {
-//       it('removes the nodes from the edges', () => {
-//         Graph.addEdges(rEdges)(n0, 1)(n1, n2, n3);
-//         Graph.removeNodes(rEdges)(n0, n2);
-//         expect(rEdges.has(n1)).toBeTrue();
-//         expect(rEdges.has(n0)).not.toBeTrue();
-//       });
-//     });
-//
-//     xdescribe('addEdges', () => {
-//       it('adds an neighbor entry for each node', () => {
-//         const nabes = Graph.addEdges(rEdges)(n0)(n1, n2, n3);
-//
-//         expect(Graph.adj(rEdges)(n0).has(n2)).toBeTrue();
-//       });
-//     });
-//
-//     xdescribe('addEntry', () => {
-//       it('appends an [node, weight] pair to the neighbrs', () => {
-//         const nabes = Graph.addNeighbor(rEdges)(n0)(n1, 3);
-//         const w0n2 = Comm.tuple(0)(n2);
-//         const newNabes = Graph.addEntry(nabes)(w0n2);
-//
-//         expect(newNabes.has(n2)).toBeTrue();
-//         expect(Graph.adj(rEdges)(n0).has(n2)).toBeFalse();
-//       });
-//     });
-//     xdescribe('addNeighbor', () => {
-//       it('adds a neigbor and weight to the src entry', () => {
-//         const nabes = Graph.addNeighbor(rEdges)(n0)(n1, 3);
-//
-//         expect(nabes.has(n1)).toBeTrue();
-//       });
-//     });
-//     xdescribe('mergeNeighbors', () => {
+  describe('addEntry', () => {
+    it('appends an [node, weight] pair to the neighbrs', () => {
+      const nabes = addNeighbor(myGraph)(n0)(n1, 3);
+      const w0n2 = tuple(0)(n2);
+      const newNabes = addEntry(nabes)(w0n2);
+
+      expect(newNabes.has(n2)).toBeTrue();
+      expect(adj(myGraph)(n0).has(n2)).toBeFalse();
+    });
+  });
+  describe('addNeighbor', () => {
+    it('adds a neigbor and weight to the src entry', () => {
+      expect((addNeighbor(myGraph)(n0)(n1, 3)).has(n1)).toBeTrue();
+    });
+  });
+
+//     describe('mergeNeighbors', () => {
 //       it('combines two neighbor maps', () => {
-//         const rNabes = Graph.addEdges(rEdges)(n0, 0)(n1, n2, n3).get(n0);
-//         const eNabes = Graph.addEdges(evens)(n2)(n5, n7, n9).get(n2);
+//         const rNabes = addEdges(rEdges)(n0, 0)(n1, n2, n3).get(n0);
+//         const eNabes = addEdges(eGraph)(n2)(n5, n7, n9).get(n2);
 //
-//         Graph.mergeNeighbors(eNabes)(rNabes);
-//         expect(Graph.adj(evens)(n2).has(n5)).toBeTrue();
+//         mergeNeighbors(eNabes)(rNabes);
+//         expect(adj(eGraph)(n2).has(n5)).toBeTrue();
 //       });
 //     });
-//     xdescribe('removeEdges', () => {
-//       it('removes an edge entry', () => {
-//         const nabes = Graph.addEdges(rEdges)(n0, 1)(n1, n2, n3);
-//
-//         Graph.removeEdges(rEdges)(n0)(n1);
-//         expect(Graph.isAdjacent(rEdges)(n0)(n1)).toBeFalse();
-//       });
-//     });
-//     xdescribe('clearNeighbors', () => {
-//       it('removes all the neighbors from the source nodes', () => {
-//         const nabes = Graph.addEdges(rEdges)(n0, 1)(n1, n2, n3);
-//
-//         Graph.clearNeighbors(rEdges)(n0, n1);
-//         expect(Graph.isAdjacent(rEdges)(n0)(n1)).toBeFalse();
-//       });
-//     });
-//     xdescribe('mergeEdges', () => {
-//       it('combines two Edge maps', () => {
-//         [ ...evens.keys(), ].map((e, id, arr) =>
-//             [ e, arr[((id + 1) % arr.length)], id, ])
-//           .reduce(Graph.addEdgeR, evens);
-//         Graph.mergeEdges(evens)(odds);
-//         expect(evens.has(n15)).toBeTrue();
-//       });
-//     });
-//   });
-// });
+  describe('removeEdges', () => {
+    it('removes an edge entry', () => {
+      const nabes = addEdges(myGraph)(n0, 1)(n1, n2, n3);
+
+      expect(isAdjacent(removeEdges(nabes)(n0)(n1))(n0)(n1)).toBeFalse();
+    });
+  });
+
+  describe('clearNeighbors', () => {
+    it('removes all the neighbors from the source nodes', () => {
+      const nGraph = addEdges(myGraph)(n0, 1)(n1, n2, n3);
+
+      expect(isAdjacent(clearNeighbors(nGraph)(n0, n1))(n0)(n1)).toBeFalse();
+    });
+  });
+
+  describe('mergeEdges', () => {
+    it('combines two Edge maps', () => {
+      const connGraph = nodes(eGraph).map((e, id, arr) =>
+            [ e, arr[((id + 1) % arr.length)], id, ]).reduce(addEdgeBin, eGraph);
+
+      // console.log('connGraph', connGraph);
+
+        // .reduce(addEdgeBin, eGraph);
+      const mGraph = mergeEdges(connGraph)(oGraph);
+
+      // console.log('mGraph', nodes(mGraph));
+
+      // console.log('eGraph', eGraph);
+      expect(mGraph.has(n7)).toBeTrue();
+    });
+  });
+});
--- src/graph.js
+++ src/graph.js
@@ -1,45 +1,53 @@
 import { collections, } from 'turmeric';
-
-const { spread, tuple, addMap, get, spreadK, flatTuple, } = collections;
+import { addEdgeBin, addSrc, clearNeighborsBin, importEdgeBin, rmEdgeBin, } from './reducers';
+const { spread, spreadKV, tuple, triple, addMap, get, spreadK, flatTuple, hasK, } = collections;
 const { uniteMap, mapDiff, mapUnion, diff } = collections;
 const { asMap, addBinMap, removeBinTuple, removeMap, removeBin } = collections;
 
-export const spawn = (edges = new Map) => new Map(edges);
+export const spawn = edges => new Map(edges);
+export const copy = spawn;
+export const fromElements = (...elements) => elements.reduce(addSrc, spawn());
+export const nodes = edges => spreadK(asMap(edges));
+export const adj = edges => src => edges.get(src) || new Map;
 
-// const fromElements = (...elements) => elements.reduce(addSrc, spawn());
+export const neighbors = edges => src => nodes(adj(edges)(src));
+
+export const contains = edges => node => hasK(edges)(node);
+
+export const isAdjacent = edges => src => nabe =>
+  contains(adj(edges)(src))(nabe);
+
+export const addNodes = edges => (...srcs) => srcs.reduce(addSrc, edges);
+
+export const removeNodes = edges => (...nodes) =>
+  nodes.reduce(removeBin, edges);
+
+export const addEdges = edges => (src, w = 0) => (...nabes) =>
+  nabes.map(triple(w)(src)).reduce(addEdgeBin, edges);
+
+export const removeEdges = edges => src => (...nabes) =>
+  nabes.map(triple(0)(src)).reduce(rmEdgeBin, edges);
+
+export const mergeEdges = (edges = new Map) => (altEdges = new Map) => {
+  const rval = spread(altEdges).reduce(importEdgeBin, edges);
+
+  // console.log('rval', rval);
+  return spreadKV(altEdges).reduce(importEdgeBin, edges);
+};
+
+export const addNeighbor = edges => src => (n, w = 0) =>
+  addBinMap(adj(edges)(src), [ n, w ]);
+export const addEntry = nabes => ([ n, w = 0 ]) => addBinMap(nabes, [ n, w ]);
+
+export const clearNeighbors = (edges = new Map) => (...nodes) =>
+  nodes.reduce(clearNeighborsBin, edges);
+
 //
-// const nodes = (edges = new Map) => spreadK(edges);
-// const adj = (edges = new Map) => (src) => edges.get(src) || new Map;
-// const neighbors = (edges = new Map) => (src) => nodes(adj(edges)(src));
-// const contains = (edges = new Map) => (node) => edges.has(node);
-// const isAdjacent = (edges = new Map) => (src) => (nabe) =>
-// 	contains(adj(edges)(src))(nabe);
+// export const clearEdges = (edges) => edges.clear;
 //
-// const addNodes = (edges = new Map) => (...srcs) => srcs.reduce(addSrc, edges);
-// const removeNodes = (edges = new Map) => (...ns) => ns.reduce(rmNode, edges);
-//
-// const addEdges = (edges = new Map) => (src, w = 0) => (...nabes) =>
-// 	nabes.map(triple(w)(src)).reduce(addEdgeR, edges);
-//
-// const removeEdges = (edges = new Map) => (src) => (...nabes) =>
-// 	nabes.map(triple(0)(src)).reduce(rmEdge, edges);
-//
-// const mergeEdges = (edges = new Map) => (altEdges = new Map) => {
-// 	spread(altEdges).reduce(importEdge, edges);
-// };
-//
-// const addEntry = (nabes = new Map) => ([n, w = 0]) => addMap(nabes, [n, w]);
-//
-// const addNeighbor = (edges = new Map) => (src) => (n, w = 0) =>
-// 	addMap(adj(edges)(src), [n, w]);
-//
-// const clearNeighbors = (edges = new Map) => (...srcs) =>
-// 	srcs.reduce(rmAdj, edges);
-//
-// const clearEdges = (edges) => edges.clear;
-//
-// const copy = spawn;
-// const mergeNeighbors = uniteMap;
+// export const copy = spawn;
+export const mergeNeighbors = uniteMap;
+
 // module.exports = {
 // 	spawn,
 // 	contains,
