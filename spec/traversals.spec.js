199378e99b8a42f9ad71229ccb7ef64f17005e2b
theory friction practice <jackson.brebnor@gmail.com>
2017-01-05 01:27:03 -0500

    rebuilds

--- spec/traversals.spec.js
+++ spec/traversals.spec.js
@@ -12,20 +12,23 @@ const { spread, spreadK } = collections;
 const altGraph = Graph.fromElements(n4, n5, n6, n7, n8, n9);
 const evenGraph = Graph.fromElements(...firstTen, ...eNodes);
 const oddGraph = Graph.fromElements(...firstTen, ...oNodes);
-let myGraph = Graph.fromElements(...firstTen);
+
+// let myGraph = Graph.fromElements(...firstTen);
+
+const myGraph = [ Graph.addEdges()(n0, 2)(n1, n2),
+  Graph.addEdges()(n1, 4)(n4, n2),
+  Graph.addEdges()(n1, 6)(n6),
+  Graph.addEdges()(n2, 3)(n3),
+  Graph.addEdges()(n5, 4)(n4),
+  Graph.addEdges()(n3, 8)(n4),
+  Graph.addEdges()(n5, 7)(n6),
+  Graph.addEdges()(n7, 7)(n8),
+  Graph.addEdges()(n0, 11)(n1), ].reduce(Graph.mergeEdgesBin, Graph.fromElements(...firstTen));
+
 const addMyGraph = Graph.addEdges(myGraph);
 const addEGraph = Graph.addEdges(evenGraph);
 const addOGraph = Graph.addEdges(oddGraph);
 
-myGraph = [ Graph.addEdges(myGraph)(n0, 2)(n1, n2),
-  Graph.addEdges(myGraph)(n1, 4)(n4, n2),
-  Graph.addEdges(myGraph)(n1, 6)(n6),
-  Graph.addEdges(myGraph)(n2, 3)(n3),
-  Graph.addEdges(myGraph)(n5, 4)(n4),
-  Graph.addEdges(myGraph)(n3, 8)(n4),
-  Graph.addEdges(myGraph)(n5, 7)(n6),
-  Graph.addEdges(myGraph)(n7, 7)(n8),
-  Graph.addEdges(myGraph)(n0, 11)(n1), ].reduce(Graph.mergeEdgesBin);
 addEGraph(n0, 22)(n9);
 addOGraph(n0, 11)(n1);
 addOGraph(n0, 22)(n9);
