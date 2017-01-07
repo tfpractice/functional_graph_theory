c4bdd8c00e511d500d4ad67e63432cec1798018a
theory friction practice <jackson.brebnor@gmail.com>
2017-01-06 19:41:48 -0500

    removes test from CI to precommit

--- __tests__/strings.spec.js
+++ __tests__/strings.spec.js
@@ -1,47 +1,46 @@
 import 'jasmine-expect';
 import { collections, } from 'turmeric';
-import { Graph, Show as Str, } from 'graph-curry';
+import { collString, edgeString, fromElements,
+  kString, kvString, pathString, redStr, vString, } from 'graph-curry';
 
 import { myNodes, } from './shared';
 const { first } = collections;
 
-const myGraph = Graph.fromElements(...myNodes);
-
 describe('strings', () => {
   describe('redStr', () => {
     it('concatenates the string with an  argument', () => {
-      expect(Str.redStr('1,2,3,', 6)).toBeString();
+      expect(redStr('1,2,3,', 6)).toBeString();
     });
   });
   describe('kString', () => {
     it('concatenates the string representaion of all keys', () => {
-      expect(Str.kString([ 1, 2, 3, 6 ])).toBeString();
+      expect(kString([ 1, 2, 3, 6 ])).toBeString();
     });
   });
 
   describe('vString', () => {
     it('concatenates the string representaion of all keys', () => {
-      expect(Str.vString(new Set([ 1, 2, 3, 6 ]))).toBeString();
+      expect(vString(new Set([ 1, 2, 3, 6 ]))).toBeString();
     });
   });
   describe('kvString', () => {
     it('concatenates the string representaion of all keys', () => {
-      expect(Str.kvString([ 1, 2, 3, 6 ])).toBeString();
+      expect(kvString([ 1, 2, 3, 6 ])).toBeString();
     });
   });
   describe('collString', () => {
     it('concatenates the string representaion of all keys', () => {
-      expect(Str.collString([ 1, 2, 3, 6 ])).toBeString();
+      expect(collString([ 1, 2, 3, 6 ])).toBeString();
     });
   });
   describe('pathString', () => {
     it('should behave...', () => {
-      expect(Str.pathString(myGraph)).toBeString();
+      expect(pathString(fromElements(...myNodes))).toBeString();
     });
   });
   describe('edgeString', () => {
     it('should behave...', () => {
-      expect(Str.edgeString(first(myGraph))).toBeString();
+      expect(edgeString(first(fromElements(...myNodes)))).toBeString();
     });
   });
 });
