c105a6c39e906b402b756c247bb23dd940c68b98
theory friction practice <jackson.brebnor@gmail.com>
2017-01-31 16:20:07 -0500

    search docs

--- src/components.js
+++ src/components.js
@@ -1,9 +1,12 @@
-import { addBinMap, diff, spreadK, spreadV, tuple, }
+import { addBinMap, addSet, diff, spreadK, spreadV, tuple, }
  from 'fenugreek-collections';
 
+ // **components** `::  Map<edge> -> Map<component>`
+ // maps each node to a set of connected nodes
 export const components = (edges) => {
   const trav = (comp = new Set, node) =>
-     diff(spreadK(edges.get(node)))(comp).reduce(trav, comp.add(node));
+     diff(spreadK(edges.get(node)))(comp).reduce(trav, addSet(comp)(node));
+     
   const visitMap = (mMap = new Map, node) =>
      diff(trav(new Set, node))(mMap).map(tuple(trav(new Set, node)))
        .reduce(addBinMap, mMap);
