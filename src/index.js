4811c9971a7470ee8ad9d868d805d53a68c16d9c
theory friction practice <jackson.brebnor@gmail.com>
2017-01-31 15:02:23 -0500

    runs doc

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ export * from './graph';
 export * from './search';
 export * from './strings';
 export * from './components';
+export * from './path';
 import * as Operations from './operations';
 export { Operations, };
