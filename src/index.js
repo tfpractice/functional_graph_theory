48b2bdcc534b3ef6b746d11084f8768c7c95ce18
theory friction practice <jackson.brebnor@gmail.com>
2017-07-03 17:43:05 -0400

    docs site

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+// exports [components](components.html)
 import * as Components from './components';
+
+// exports [contract](contract.html)
 import * as Contract from './contract';
+
+// exports [graph](graph.html)
 import * as Graph from './graph';
+
+// exports [path](path.html)
 import * as Path from './path';
+
+// exports [reducers](reducers.html)
 import * as Reducers from './reducers';
+
+// exports [search](search.html)
 import * as Search from './search';
+
+// exports [show](show.html)
 import * as Show from './show';
 
 export { Components, Contract, Graph, Path, Search, Show, };
