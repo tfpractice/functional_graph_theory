4f602b9dec9d9a0549ae437750dee1a0e296b3b0
theory friction practice <jackson.brebnor@gmail.com>
2017-08-10 16:48:24 -0400

    contraact changes

--- src/contract.js
+++ src/contract.js
@@ -1,10 +1,19 @@
-import { addMap, asMap, asSet, first, flatten, flattenBin, isIterable,
-   removeBin, spread, } from 'fenugreek-collections';
+import {
+  addMap,
+  asMap,
+  asSet,
+  first,
+  flatten,
+  flattenBin,
+  isIterable,
+  removeBin,
+  spread,
+} from 'fenugreek-collections';
 import { adj, copy, mergeEdges, neighbors, nodes, removeNodes, } from './graph';
 import * as reducers from './reducers';
 
 export const autoSpread = el =>
-isIterable(el) ? (spread(el).reduce(flattenBin, []).map(autoSpread)) : el;
+  isIterable(el) ? spread(el).reduce(flattenBin, []).map(autoSpread) : el;
 
 export const superNode = src => nb => asSet([ src, nb ]);
 
@@ -15,22 +24,21 @@ export const combineAdj = g => src => nb =>
   asMap(flatten(adj(g)(src))(adj(g)(nb)));
 
 export const superAdj = g => src => nb =>
- [ src, nb ].reduce(removeBin, combineAdj(g)(src)(nb));
+  [ src, nb ].reduce(removeBin, combineAdj(g)(src)(nb));
 
 export const superEdge = g => src => nb =>
   addMap()(superNode(src)(nb))(superAdj(g)(src)(nb));
 
 export const contract = g => src => (nb = first(neighbors(g)(src))) =>
-nb ? mergeEdges(removeNodes(g)(src, nb))(superEdge(g)(src)(nb)) : g;
+  nb ? mergeEdges(removeNodes(g)(src, nb))(superEdge(g)(src)(nb)) : g;
 
 export const contractBin = (g, [ src, nb ]) => contract(g)(src)(nb);
 
 export const contractSrc = g => src =>
-reducers.neighborPairs(g)(src).reduce(contractBin, copy(g));
+  reducers.neighborPairs(g)(src).reduce(contractBin, copy(g));
 
-export const contractNext = (g, n = (first(nodes(g)))) =>
-  contract(copy(g))(n)();
+export const contractNext = (g, n = first(nodes(g))) => contract(copy(g))(n)();
 
 export const contractAuto = g => nodes(g).reduce(contractNext, g);
 export const contractMin = (g, min = 2) =>
- g.size > min ? contractMin(contractNext(g), min) : copy(g);
+  g.size > min ? contractMin(contractNext(g), min) : copy(g);
